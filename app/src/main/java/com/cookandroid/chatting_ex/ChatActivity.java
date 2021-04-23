b485d1b54d07f49efcec9a00b7d9aa72b63817b8
taehee <kth25532@naver.com>
2021-04-23 21:33:12 +0900

    Add code on ChatActivity.java - Write data on Realtime DB

--- app/src/main/java/com/cookandroid/chatting_ex/ChatActivity.java
+++ app/src/main/java/com/cookandroid/chatting_ex/ChatActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        //1. recyclerView - 반복
+        //2. 디비 내용을 넣는다
+        //3. 상대방 폰에 채팅 내용이 보임
     }
 }
