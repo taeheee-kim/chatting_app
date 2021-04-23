59aa169513dd98a58bdcac3a4b24faf5189d07b3
taehee <kth25532@naver.com>
2021-04-23 21:59:45 +0900

    Modify ChatActivity.java and activity_chat.xml
    Make EditText and Button
    if user types comment and press the button,
    String data is transmitted to Firebase DB.

--- app/src/main/java/com/cookandroid/chatting_ex/ChatActivity.java
+++ app/src/main/java/com/cookandroid/chatting_ex/ChatActivity.java
@@ -3,22 +3,40 @@ package com.cookandroid.chatting_ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
 
+    EditText GetEmail;
+    String email;
+    Button Finish;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        GetEmail = findViewById(R.id.GetEmail);
+        Finish = findViewById(R.id.Finish);
 
-        myRef.setValue("Hello, World!");
+        Finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = GetEmail.getText().toString();
+
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue(email);
+            }
+        });
+
 
         //1. recyclerView - 반복
         //2. 디비 내용을 넣는다
